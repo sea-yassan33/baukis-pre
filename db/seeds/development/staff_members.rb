574b6e443a4f5ad15d0b464bb637f65c48b78251
sea-yassan33 <a.yasuda_tr51@me.com>
2023-02-25 17:21:29 +0900

    シードデータ

---
+++ db/seeds/development/staff_members.rb
@@ -0,0 +1,9 @@
+StaffMember.create!(
+  email: 'test@example.com',
+  family_name: 'test',
+  given_name: '太郎',
+  family_name_kana: 'テスト',
+  given_name_kana: 'タロウ',
+  password: 'password',
+  start_date: Date.today
+)
